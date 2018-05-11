f21d5a563e13cd3ed413665c9a49ba2326f94f8c
Cristhian Leon <Cristhian.X.Leon.-ND@disney.com>
2018-05-10 21:14:31 -0500

    Initial Commit

---
+++ CameraView/CameraView.h
@@ -0,0 +1,19 @@
+//
+//  CameraView.h
+//  CameraView
+//
+//  Created by Cristhian León on 5/10/18.
+//  Copyright © 2018 Cristhian León. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraView.
+FOUNDATION_EXPORT double CameraViewVersionNumber;
+
+//! Project version string for CameraView.
+FOUNDATION_EXPORT const unsigned char CameraViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraView/PublicHeader.h>
+
+
